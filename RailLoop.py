2e4238690161769b6cf7d93d2acca51ed6b43c0c
GloriousRedLeader <arturotheburro7@gmail.com>
2025-03-04 01:24:02 -0800

    updating rail docs

--- RailLoop.py
+++ RailLoop.py
@@ -22,11 +22,14 @@ Player.HeadMessage(48, "Starting Rail Loop")
 # It is recommended to make those routes a loop that start and end at or
 # around the same coordinate since it loops indefinitely.
 # You can find a list of coordinates predefined in fm_core/core_routes.py
+# If an enemy is found within attackRange (configued below), your character will move
+# to that enemy. It will follow it until it is dead or becomes unreachable.
+# Use this script in conjunction with AttackLoop or CasterLoop which will do your offense.
 run_rail_loop(
 
     # (Required) This is a list of coordinates to travel. See core_routes for a list of available, pre-defined routes.
-    # You can generate your own using the rails tool. It's easy. Just load up the script in fm_tools/RailRecorder.py
-    # and start adding points. Walk to a location, click add point. When you're done hit save. Open the file. It 
+    # You can generate your own using the rails tool. Its easy. Just load up the script RailRecorder.py
+    # and start adding points. Walk to a location, click add point. When youre done hit save. Open the file. It 
     # will contain a list of coordinates you can paste here. Your character will walk around like an idiot.
     path = new_haven_noob_dungeon,
 
@@ -35,15 +38,17 @@ run_rail_loop(
     attackRange = 5,    
     
     # (Optional) Number of seconds to allow before giving up when going from one coord to another.
-    # Default is 3 seconds.
+    # Default is 5 seconds.
     pathFindingTimeoutSeconds = 5.0,
     
     # (Optional) Give a little extra time to loot when a monster dies. This is useful. A nice value
-    # is about 2000ms.
-    autoLootBufferMs = 1000,
+    # is about 2 seconds.
+    autoLootBufferMs = 2000,
     
-    # (Optional) Value of 0 means land right on x, y. This is the default behavior. Positive value means stop 
-    # short of the provided x, y by that many tiles. This is useful for casters or anyone who 
+    # (Optional) When a mob is found, your character will move right on top of it. This value governs
+    # whether you should stop tileOffset tiles before it. This is useful for casters or anyone who 
     # doesnt wish to be directly on top of a mobile.
-    tileOffset = 3
+    # A value of 0 (default) means land right on the target x, y. Positive value means stop 
+    # short of the provided x, y by that many tiles. 
+    tileOffset = 0 
 )
