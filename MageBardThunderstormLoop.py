3318d3d3405a294caffb7f9fb3a1fdd34b6deb22
GloriousRedLeader <arturotheburro7@gmail.com>
2025-08-21 11:31:01 -0700

    renaming forclarit praise mao

--- MageBardThunderstormLoop.py
+++ MageBardThunderstormLoop.py
@@ -26,7 +26,7 @@ run_mage_loop(
 
     # Give it a fun name in case you have different versions, e.g.
     # Mage AOE Loop or Mage Single Target Loop
-    loopName = "Bard Mage AoE",
+    loopName = "Mage Bard AoE",
 
     # Applicable when heal spells are enabled (Greater Heal, etc.) which are configured
     # down below. This governs how you want to heal allies. By default your character is always healed.
